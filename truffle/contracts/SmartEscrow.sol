91595fb867d05dbe0943cba3e448669b3e3230bc
ryunosuke121 <ryunosuke.ito@horizon-cg.com>
2022-12-13 13:19:56 +0900

    add escrow function

--- truffle/contracts/SmartEscrow.sol
+++ truffle/contracts/SmartEscrow.sol
@@ -1,11 +1,51 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-contract SmartEscrow {
-    address public owner;
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+import "../node_modules/@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "../node_modules/@openzeppelin/contracts/utils/escrow/RefundEscrow.sol";
 
-    constructor(address _owner){
-        owner = _owner;
+contract SmartEscrow is Ownable{
+    using SafeMath for uint256;
+
+    uint256 public unitPrice;
+    uint256 public deadline;
+    uint32 public requestNumber;
+    string public url;
+    uint256 public totalDeposit;
+
+    //アドレスへの支払いが許可されているかどうか
+    mapping(address => bool) public isWithdrawalAllowed;
+
+    //一人当たりの単価と締切、ページのURLをパラメーターとしてコントラクトを初期化する
+    constructor(
+        uint256 _unitPrice,
+        uint256 _deadline,
+        uint32 _requestNumber, 
+        string memory _url
+        )
+        {
+        unitPrice = _unitPrice;
+        deadline = _deadline;
+        requestNumber = _requestNumber;
+        url = _url;
     }
 
+    //コントラクトに預け入れるための関数
+    function makeDeposit() public payable onlyOwner{
+        require(unitPrice * requestNumber <= msg.value, "funds are in short supply");
+        totalDeposit = totalDeposit.add(msg.value);
+    }
+
+    //payeeへの引き出しが許可されていれば、payeeに対してunitPrice分のEtherを送る
+    function withdraw(address payable payee) public {
+        require(isWithdrawalAllowed[payee] == true, "withdrawal is not allowed");
+        isWithdrawalAllowed[payee] = false;
+        totalDeposit = totalDeposit.sub(unitPrice);
+        payee.transfer(unitPrice);
+    }
+    //payeeによる引き出しを許可する関数
+    function allowWithdrawal(address payee) public onlyOwner{
+        isWithdrawalAllowed[payee] = true;
+    }
 }
--- truffle/test/SmartEscrow_test.js
+++ truffle/test/SmartEscrow_test.js
@@ -2,18 +2,97 @@ const SmartEscrowContract = artifacts.require("SmartEscrow");
 
 contract("SmartEscrow", (accounts) => {
     let smartEscrow;
+    const unitPrice = web3.utils.toWei('0.3');
+    const deadline = 3600;
+    const reqNumber = 5;
+    const url = "beneficiaryname.org";
     const owner = accounts[0];
+    const payee = accounts[1];
 
     describe("initialization", () => {
 
         beforeEach(async () => {
-            smartEscrow = await SmartEscrowContract.new(owner);
+            smartEscrow = await SmartEscrowContract.new(
+                unitPrice,
+                deadline,
+                reqNumber,
+                url
+            );
         });
 
-        it("gets the beneficialy owner", async () => {
-            const actual = await smartEscrow.owner();
-            assert.equal(actual, owner, "owner should match");
+        it("gets the beneficialy unitPrice", async () => {
+            const actual = await smartEscrow.unitPrice();
+            assert.equal(actual, unitPrice, "unitPrice should match");
+        });
+
+        it("gets the beneficialy deadline ", async () => {
+            const actual = await smartEscrow.deadline();
+            assert.equal(actual, deadline, "deadline should match");
+        });
+
+        it("gets the beneficialy reqNumber", async () => {
+            const actual = await smartEscrow.requestNumber();
+            assert.equal(actual, reqNumber, "reqNumber should match");
+        });
+
+        it("gets the beneficialy url", async () => {
+            const actual = await smartEscrow.url();
+            assert.equal(actual, url, "url should match");
+        });
+
+    });
+
+    describe("make deposits", () => {
+        
+        const value = web3.utils.toWei('2.5');
+
+        it("increase the totalDeposit amount", async () => {
+            const currentDeposit = await smartEscrow.totalDeposit();
+            await smartEscrow.makeDeposit({
+                from: owner,
+                value: value
+            });
+            const newDeposit = await smartEscrow.totalDeposit();
+            const diff = newDeposit - currentDeposit;
+            assert.equal(diff, value, "diff should match");
         });
     });
 
+    describe("withdraw unitPrice from deposit", () => {
+
+        it("throws error when payee is not allowed", async () => {
+            try{
+                await smartEscrow.withdraw(payee,{from: owner});
+                assert.fail("withdraw is not restricted");
+            }catch(err){
+                const expectedError = "withdrawal is not allowed";
+                const actualError = err.reason;
+                assert.equal(actualError, expectedError, "should not be withdrawn");
+            }
+        });
+
+        it("allow withdrawal from payee", async () => {
+            await smartEscrow.allowWithdrawal(payee, {from: owner});
+            const isWithdrawalAllowed = await smartEscrow.isWithdrawalAllowed(payee);
+            assert.equal(isWithdrawalAllowed, true, "should be true");
+        });
+
+        it("withdraw unitPrice", async () => {
+            const currentPayeeBalance = await web3.eth.getBalance(payee);
+            //{from:payee}とすると、ガス代の支払い元がpayeeになるので、一致しなくなる
+            await smartEscrow.withdraw(payee,{from: owner});
+            const newPayeeBalance = await web3.eth.getBalance(payee);
+            const diffPayeeBalance = newPayeeBalance - currentPayeeBalance;
+            assert.equal(unitPrice, diffPayeeBalance, "should match");
+        });
+
+        it("subtract unitPrice from totalDeposit", async () => {
+            await smartEscrow.allowWithdrawal(payee, {from: owner});
+            const currentTotalDeposit = await smartEscrow.totalDeposit();
+            await smartEscrow.withdraw(payee,{from: owner});
+            const newTotalDeposit = await smartEscrow.totalDeposit();
+            const diffTotalDeposit = currentTotalDeposit - newTotalDeposit;
+            assert.equal(diffTotalDeposit, unitPrice, "should match"); 
+        });
+    });
 })
